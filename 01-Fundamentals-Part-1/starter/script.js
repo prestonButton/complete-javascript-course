f9bba0f1d70027b8146e88b7678679f88fb3c019
prestonButton <prestonbutton99@gmail.com>
2022-12-09 17:56:00 -0500

    Part1; lecture2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -2,3 +2,11 @@ let country = "United States";
 let continent = "North America";
 let population = 328.2;
 console.log(country);
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
