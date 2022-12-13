7c561f1249426a6fab1d4b0bf36650b47b8800ec
prestonButton <prestonbutton99@gmail.com>
2022-12-12 20:45:02 -0500

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -15,3 +15,9 @@ const applejuice = fruitProcessor(5, 0);
 console.log(applejuice);
 
 console.log(fruitProcessor(3, 9));
+
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} million people and its capital city is ${capitalCity}.`
+}
+
+console.log(describeCountry('USA', 328, 'DC'));
