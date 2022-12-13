caf27db2437b996533bd17e456596f42bc03cd88
prestonButton <prestonbutton99@gmail.com>
2022-12-12 21:23:32 -0500

    arrow Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -32,16 +32,31 @@ const calcAge2 = function(birthYear) {
     return 2022 - birthYear;
 }
 
-console.log(calcAge2(2002));
+// console.log(calcAge2(2002));
 
 function percentageOfWorld1(population){
     return (population/79);
 }
 
-console.log(percentageOfWorld1(1441));
+// console.log(percentageOfWorld1(1441));
 
 const percentageOfWorld2 = function(population) {
     return (population/79);
 }
 
-console.log(percentageOfWorld2(790));
+// console.log(percentageOfWorld2(790));
+
+const calcAge3 = birthYear => 2022 - birthYear;
+
+// console.log(calcAge3(2001))
+
+const ageToRetirement = birthYear => {
+    const currentAge = 2022 - birthYear;
+    return 65 - currentAge;
+}
+
+console.log(ageToRetirement(2001));
+
+const percentageOfWorld3 = population => population/79;
+
+console.log(percentageOfWorld3(790));
