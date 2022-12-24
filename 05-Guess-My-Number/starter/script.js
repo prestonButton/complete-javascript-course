4d380d56aa91eaa9aed367701baa541a8869ec5d
prestonButton <prestonbutton99@gmail.com>
2022-12-24 14:36:16 -0500

    Video 74

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,12 +10,41 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);*/
 
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value)
 
   console.log(guess, typeof guess);
 
-  if(!guess) {
+  if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number';
+  } else if (guess < 0 || guess > 20) {
+    document.querySelector('.message').textContent = '❌ Invalid Guess';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number';
+  } else if (guess > secretNumber) {
+    if(score > 1) {
+      document.querySelector('.message').textContent = '🔻 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🗑️ You Lose';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🔺 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🗑️ You Lose';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
   }
 });
