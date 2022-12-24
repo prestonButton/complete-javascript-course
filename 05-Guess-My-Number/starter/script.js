8ee276d4702987991ffff8edcbe1c38de826a742
prestonButton <prestonbutton99@gmail.com>
2022-12-24 14:45:34 -0500

    Manipulating styles Vid 75

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,24 +10,35 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);*/
 
-const secretNumber = Math.trunc(Math.random()*20) + 1;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
 
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value)
+  const guess = Number(document.querySelector('.guess').value);
 
   console.log(guess, typeof guess);
 
+  //when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number';
+
+    //when input is outside of scope of game
   } else if (guess < 0 || guess > 20) {
     document.querySelector('.message').textContent = '❌ Invalid Guess';
+
+    //when user wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    //if guess is too high
   } else if (guess > secretNumber) {
-    if(score > 1) {
+    if (score > 1) {
       document.querySelector('.message').textContent = '🔻 Too High';
       score--;
       document.querySelector('.score').textContent = score;
@@ -36,6 +47,8 @@ document.querySelector('.check').addEventListener('click', function () {
       score--;
       document.querySelector('.score').textContent = score;
     }
+
+    //if guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '🔺 Too Low';
