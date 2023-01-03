d8143c757623690572379526e3f3f25a91a453ee
prestonButton <prestonbutton99@gmail.com>
2023-01-03 11:42:15 -0500

    scoping

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function calcAge(birthyear) {
+    const age = 2037 - birthyear;
+
+    function printAge(){
+        const output = `${firstName}, you are ${age}, born in ${birthyear}`;
+        console.log(output);
+
+        if(birthyear >= 1981 && birthyear <= 1986) {
+            const str = `Oh, and you are a millenial, ${firstName}`;
+            console.log(str);
+
+            function add(a, b) {
+                return a + b;
+            }
+        }
+    }
+    printAge();
+
+    return age;
+}
+
+const firstName = 'Preston';
+calcAge(2001);
