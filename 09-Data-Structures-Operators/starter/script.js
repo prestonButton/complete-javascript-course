1bd6728c6d01fb4429fdafe8386cc1a73ebcca71
prestonButton <prestonbutton99@gmail.com>
2023-01-04 10:59:55 -0500

    destructuring objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -27,17 +27,57 @@ const restaurant = {
     },
   },
 
-  order: function(starterIndex, mainIndex) {
+  order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+    console.log(
+      `Order received!: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`
+    );
   }
 };
 
-const [starter, mainCourse] = (restaurant.order(2, 0));
+restaurant.orderDelivery({
+  time: '23:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+})
+
+restaurant.orderDelivery({
+  address: '101 NMR Dr.',
+  starterIndex: 1
+})
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
+
+const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
 
 const nested = [2, 4, [5, 6]];
 // const [i,, j] = nested;
 // console.log(i, j);
 
-const [i,, [j, k]] = nested;
+const [i, , [j, k]] = nested;
 console.log(i, j, k);
