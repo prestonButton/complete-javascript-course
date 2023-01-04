6174446ef1049aa376eed49c03a2676f0c8ef0d0
prestonButton <prestonbutton99@gmail.com>
2023-01-03 20:04:31 -0500

    JS behind the screen finished

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -33,3 +33,23 @@ const year = 2001;
 function addDecl(a, b){return a + b;};
 const addExpr = function(a, b) {return a+b};
 const addArrow = (a, b) => a + b;
+
+
+const getAge = function(birthyear) {
+    console.log(2022 - birthyear);
+    console.log(this);
+}
+
+getAge(2007 );
+
+const preston = {
+    firstName: "preston",
+    lastName: 'Button',
+    birthyear: 2001,
+    family: ['Ev', 'Luke']
+};
+
+const prestonCopy = Object.assign({}, preston);
+
+prestonCopy.firstName = 'Preston';
+console.log(prestonCopy);
