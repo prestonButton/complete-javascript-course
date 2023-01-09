cbac038a4b2814f6f4fb634d7f354860e7972e1d
prestonButton <prestonbutton99@gmail.com>
2023-01-09 12:29:38 -0500

    Optional Chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -54,6 +53,27 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon);
+
+//with optional chaining
+
+console.log(restaurant.openingHours?.mon?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for(const day of days){
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(open);
+}
+
+//methods
+
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+//arrays
+const users = [{name: 'Preston', email: 'me@preston.com'}];
+console.log(users[0]?.name ?? 'User array empty');
+
 
 // const rest1 = {
 //   name: 'capri',
