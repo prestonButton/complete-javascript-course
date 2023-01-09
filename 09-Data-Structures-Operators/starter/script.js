b5ef44c518c52639b019808946d4bc12e028cd1b
prestonButton <prestonbutton99@gmail.com>
2023-01-09 12:11:49 -0500

    enhanced object literals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,24 @@
 'use strict';
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 
 // Data needed for first part of the section
 const restaurant = {
@@ -12,26 +28,13 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  openingHours,
 
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({
+  orderDelivery({
     starterIndex = 1,
     mainIndex = 0,
     time = '20:00',
@@ -42,43 +45,43 @@ const restaurant = {
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
 };
 
-const rest1 = {
-  name: 'capri',
-  // numGuests: 20,
-  numGuests: 0,
-}
+// const rest1 = {
+//   name: 'capri',
+//   // numGuests: 20,
+//   numGuests: 0,
+// }
 
-const rest2 = {
-  name: 'La Pieta',
-  owner: 'Gio Rossi',
-};
+// const rest2 = {
+//   name: 'La Pieta',
+//   owner: 'Gio Rossi',
+// };
 
-//or assignment operator
-// rest1.numGuest = rest1.numGuests || 10;
-// rest2.numGuest = rest2.numGuests || 10;
+// //or assignment operator
+// // rest1.numGuest = rest1.numGuests || 10;
+// // rest2.numGuest = rest2.numGuests || 10;
 
-rest1.numGuests ??= 10;
-rest2.numGuests ??= 10;
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
 
-//and assignment operator
-// rest2.owner = rest2.owner && '<ANONYMOUS>';
-// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// //and assignment operator
+// // rest2.owner = rest2.owner && '<ANONYMOUS>';
+// // rest1.owner = rest1.owner && '<ANONYMOUS>';
 
-rest1.owner &&= '<ANONYMOUS>';
-rest2.owner &&= '<ANONYMOUS>';
+// rest1.owner &&= '<ANONYMOUS>';
+// rest2.owner &&= '<ANONYMOUS>';
 
-console.log(rest1);
-console.log(rest2);
+// console.log(rest1);
+// console.log(rest2);
 
 // console.log('---OR---');
 // console.log('' || 'Preston');
