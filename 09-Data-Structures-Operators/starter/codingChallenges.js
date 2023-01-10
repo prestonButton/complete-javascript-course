150212a4739cd5b8fdc95f4f398c8888a61d22a7
prestonButton <prestonbutton99@gmail.com>
2023-01-10 18:10:50 -0500

    add notes sheet

--- 09-Data-Structures-Operators/starter/codingChallenges.js
+++ 09-Data-Structures-Operators/starter/codingChallenges.js
@@ -61,7 +61,7 @@ const game = {
     ],
   ],
   score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  scored: ['Lewandowski', 'Gnabry', 'Lewandowski', 'Hummels'],
   date: 'Nov 9th, 2037',
   odds: {
     team1: 1.33,
@@ -70,8 +70,8 @@ const game = {
   },
 };
 
-// 1. Create one player array for each team (variables 'players1' and 
-// 'players2') 
+// 1. Create one player array for each team (variables 'players1' and
+// 'players2')
 
 const players1 = game.players[0];
 const players2 = game.players[1];
@@ -79,50 +79,92 @@ const players2 = game.players[1];
 console.log(`Players1: ${players1}`);
 console.log(`Players2: ${players2}`);
 
-// //2. The first player in any player array is the goalkeeper and the others are field 
-// players. For Bayern Munich (team 1) create one variable ('gk') with the 
-// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 
-// field players 
+// //2. The first player in any player array is the goalkeeper and the others are field
+// players. For Bayern Munich (team 1) create one variable ('gk') with the
+// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+// field players
 
 const [gk, ...fieldPlayers] = players1;
 console.log(`GK: ${gk}`);
 console.log(`Field Players: ${fieldPlayers}`);
 
-// 3. Create an array 'allPlayers' containing all players of both teams (22 
-// players) 
+// 3. Create an array 'allPlayers' containing all players of both teams (22
+// players)
 
 const allPlayers = [...players1, ...players2];
 console.log(allPlayers);
 
-// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a 
-// new array ('players1Final') containing all the original team1 players plus 
-// 'Thiago', 'Coutinho' and 'Perisic' 
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+// new array ('players1Final') containing all the original team1 players plus
+// 'Thiago', 'Coutinho' and 'Perisic'
 
 const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
 console.log(players1Final);
 
-// 5. Based on the game.odds object, create one variable for each odd (called 
-// 'team1', 'draw' and 'team2') 
+// 5. Based on the game.odds object, create one variable for each odd (called
+// 'team1', 'draw' and 'team2')
 
-const {odds: {team1, x: draw, team2}} = game;
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
 console.log(`Team1: ${team1}`);
 console.log(`draw: ${draw}`);
 console.log(`Team2: ${team2}`);
 
-// 6. Write a function ('printGoals') that receives an arbitrary number of player 
-// names (not an array) and prints each of them to the console, along with the 
-// number of goals that were scored in total (number of player names passed in) 
+// 6. Write a function ('printGoals') that receives an arbitrary number of player
+// names (not an array) and prints each of them to the console, along with the
+// number of goals that were scored in total (number of player names passed in)
 
-const printGoals = function(...names) {
-    console.log(names, `${names.length} goals were scored`);
-}
+const printGoals = function (...names) {
+  console.log(names, `${names.length} goals were scored`);
+};
 
 printGoals('Davies', 'Muller', 'Lewandowski');
-printGoals(...game.scored)
+printGoals(...game.scored);
 
-// 7. The team with the lower odd is more likely to win. Print to the console which 
-// team is more likely to win, without using an if/else statement or the ternary 
+// 7. The team with the lower odd is more likely to win. Print to the console which
+// team is more likely to win, without using an if/else statement or the ternary
 // operator.
 
-team1 < team2 && console.log(`Team1 is more likely to win`); 
-team1 > team2 && console.log(`Team2 is more likely to win`); 
+team1 < team2 && console.log(`Team1 is more likely to win`);
+team1 > team2 && console.log(`Team2 is more likely to win`);
+
+/*
+Coding Challenge #2 
+Let's continue with our football betting app! Keep using the 'game' variable from 
+before. 
+Your tasks: 
+1. Loop over the game.scored array and print each player name to the console, 
+along with the goal number (Example: "Goal 1: Lewandowski") 
+2. Use a loop to calculate the average odd and log it to the console (We already 
+studied how to calculate averages, you can go check if you don't remember) 
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this: 
+Odd of victory Bayern Munich: 1.33 
+Odd of draw: 3.25 
+ Odd of victory Borrussia Dortmund: 6.5 
+Get the team names directly from the game object, don't hardcode them 
+(except for "draw"). Hint: Note how the odds and the game objects have the 
+same property names 😉 
+4. Bonus: Create an object called 'scorers' which contains the names of the 
+players who scored as properties, and the number of goals as the value. In this 
+game, it will look like this: 
+{ 
+  Gnarby: 1, 
+  Hummels: 1, 
+  Lewandowski: 2 
+} 
+ 
+GOOD LUCK 😀 
+*/
+
+// 1. Loop over the game.scored array and print each player name to the console,
+// along with the goal number (Example: "Goal 1: Lewandowski")
+console.log(game.scored.entries());
+for (const [index, player] of game.scored.entries()) console.log(`Goal ${index + 1}: ${player}`);
+
+// 2. Use a loop to calculate the average odd and log it to the console (We already
+// studied how to calculate averages, you can go check if you don't remember)
+let average = 0;
+for(const [key, value] of Object.entries(game.odds)){
+  console.log(`Key: ${key}, Value: ${value}`);
+}
---
+++ CompleteJScourseNotes.md
@@ -0,0 +1,20 @@
+<hr>
+<h1>Data Structures, Modern Operators and Strings</h1>
+<hr>
+
+<br>
+
+<h2>Destructuring Arrays</h2>
+<p>
+    Destructuring was introduced in ES6, used to unpack values from objects, including arrays, into seperate variables.
+
+    We used to:
+    <code>
+        const arr = [2, 3, 4, 5, 6, 7];
+        const a = arr[0];
+        const b = arr[1]
+    </code>
+    
+    etc. 
+
+</p>
